33bbc9a34339a8b3f1e374a24c43f88cd6ec7e87
MajeedKazemi <majeed@dgp.toronto.edu>
2022-06-20 16:24:44 -0400

    fixed trailing zeros in the displayed time

--- packages/client/src/utils/shared.ts
+++ packages/client/src/utils/shared.ts
@@ -13,5 +13,11 @@ export function convertTime(seconds: number): string {
     const m = Math.floor(seconds / 60);
     const s = Math.floor(seconds % 60);
 
-    return `${m}:${s}`;
+    let sStr = s.toString();
+    let mStr = m.toString();
+
+    if (s < 10) sStr = "0" + sStr;
+    if (m < 10) mStr = "0" + mStr;
+
+    return `${mStr}:${sStr}`;
 }
